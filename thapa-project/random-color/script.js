438c3bfd593ceec44ae7764d0fe6ad83acf66d25
bhagwat dixit <Bhagwatdixit55@gmail.com>
2024-03-11 17:18:25 +0530

    random color

---
+++ thapa-project/random-color/script.js
@@ -0,0 +1,5 @@
+const myBtn1 = document.querySelector('#btn1')
+const myBtn2 = document.querySelector('#btn2')
+
+myBtn1.addEventListener('click', handleButton1);
+myBtn2.addEventListener('click', handleButton2)
